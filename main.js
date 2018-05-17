dbe358c328098802293c30922daa7063508cdeea
amcdevitt97 <amcdevitt97@gmail.com>
2018-05-17 17:48:08 -0400

    I made the "expand" button prettier by making it an emoji
    design smarter not harder

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ function showTable(div) {
     var x = document.getElementById(div);
     if (x.style.display === "block") {
         x.style.display = "none";
-        document.getElementById("CSbutton").innerHTML = "Show";
+        document.getElementById("CSbutton").innerHTML = "🔽";
     }
     else if( x.style.display === "none") {
         x.style.display = "block";
-        document.getElementById("CSbutton").innerHTML = "Collapse";
+        document.getElementById("CSbutton").innerHTML = "🔼";
     }
 }
 
